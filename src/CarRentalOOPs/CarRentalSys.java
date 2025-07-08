27a1566e1c817d6fc66cc28f73f34ce3f79e0211
Siddharth28004 <siddharth2814@gmail.com>
2025-07-08 14:10:55 +0530

    Car rental system.

--- src/CarRentalOOPs/CarRentalSys.java
+++ src/CarRentalOOPs/CarRentalSys.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class CarRentalSys {
 
-    class Car{
+    static class Car{
         private String carID;
         private String brand;
         private String model;
@@ -42,7 +42,7 @@ public class CarRentalSys {
 
     }
 
-    class Rental{
+    static class Rental{
         private Customer  customer;
         private Car car;
         private int days;
@@ -61,7 +61,7 @@ public class CarRentalSys {
 
     }
 
-    class Customer{
+    static class Customer{
         private String customername;
         private String customerId;
 
@@ -78,12 +78,12 @@ public class CarRentalSys {
         }
     }
 
-    class RentalSystem{
+     static class RentalSystem{
         private List <Car> cars;
         private List <Customer> customers;
         private List <Rental> rentals;
 
-        public RentalSystem(){
+        public RentalSystem(){ //creating array lists to store
             cars= new ArrayList<>();
             customers=new ArrayList<>();
             rentals=new ArrayList<>();
@@ -101,8 +101,8 @@ public class CarRentalSys {
 
         public void rentCar(Car car, Customer customer, int days){
             if(car.isAvailable()){
-                car.rent();
-                rentals.add(new Rental(customer, car,days));
+                car.rent();//makes it false for future use
+                rentals.add(new Rental(customer, car,days)); //adding the infor to rentals arraylist
             }
             else{
                 System.out.println("Car is not available for rent");
@@ -111,9 +111,9 @@ public class CarRentalSys {
         }
 
         public void returnCar(Car car){
-            car.returnCar();
+            car.returnCar();//is an attrib of the Car class
             Rental rentaltoremove=null;
-            for( Rental rental: rentals){
+            for( Rental rental: rentals){ //traversing the arraylist
                 if(rental.getCar()==car){
                     rentaltoremove=rental;
                     break;
@@ -140,6 +140,7 @@ public class CarRentalSys {
                 System.out.print("Enter your choice: ");
 
                 int choice= scanner.nextInt();
+                scanner.nextLine();
 
                 if(choice==1){
                     System.out.println("\n===Rent a Car===\n");
@@ -160,7 +161,7 @@ public class CarRentalSys {
                     int rentaldays= scanner.nextInt();
                     scanner.nextLine();
 
-                    Customer newcustomer= new Customer("CUS"+(customers.size()+1), customername);
+                    Customer newcustomer= new Customer("CUS"+(customers.size()+1), customername);//refer customer class for attribs
                     addCustomer(newcustomer);
 
                     Car selectedcar=null;
@@ -172,30 +173,98 @@ public class CarRentalSys {
                     }
 
                     if(selectedcar!=null){
-                        double total price= selectedcar.calculatetotalprice(rentaldays);//part of Car class attribs
+                        double totalprice= selectedcar.calculatetotalprice(rentaldays);//part of Car class attribs
+                        System.out.println("\n===Rental Information==\n");
+                        System.out.println("Customer ID: "+newcustomer.getCustomerId());
+                        System.out.println("Customer Name: "+newcustomer.getCustomername());
+                        System.out.println("Car: "+selectedcar.getBrand()+" "+selectedcar.getModel());
+                        System.out.println("Rental days: "+rentaldays);
+                        System.out.printf("Total price:₹%.2f%n",totalprice);/*  okay so here what it means is first i used ₹ symbol
+                        then  %.2f here % starts it .2 means upto 2decimal place and f means float and %n newline can use/n also */
+                        System.out.println("\n Confirm your rental (Y/N): ");
+                        String confirm=scanner.nextLine();
+
+                        if(confirm.equalsIgnoreCase("Y")){ //basically using equals with Ignore case ensures case insensitivity
+                            rentCar(selectedcar,newcustomer,rentaldays); //constructor part of Rental System class
+                            System.out.println("\n Car rented successfully!");
+
+                        }
+                        else{
+                            System.out.println("\n Rental Cancelled");
+
+                        }
+                    }
+                    else{
+                        System.out.println("Invalid selection or Car is not available to rent");
 
                     }
 
+                }
 
+                else if(choice==2){
+                    System.out.println("\n===Return a Car===");
+                    System.out.println("Enter the ID of the car you want to return: ");
+                    String carId= scanner.nextLine();
 
+                    Car cartoreturn=null;
 
+                    for(Car car:cars){
+                        if(car.getCarID().equals(carId)&&!car.isAvailable()){
+                            cartoreturn=car;
+                            break;
+                        }
+                    }
 
+                    if(cartoreturn!=null){
+                        Customer customer=null;
+                        for(Rental rental: rentals){
+                            if(rental.getCar()==cartoreturn){
+                                customer=rental.getCustomer();
+                                break;
+                            }
 
+                        }
+                        if(customer!=null){
+                            returnCar(cartoreturn);
+                            System.out.println("Car returned successfully by: "+customer.getCustomername());
+                        }
+                        else{
+                            System.out.println("Car was not rented or some information provided is missing");
+
+                        }
+                    }
+
+                    else{
+                        System.out.println("Invalid Car ID or Car was not rented");
+                    }
+
+                }
+
+                else if(choice==3){
+                    break;
+                }
+
+                else{
+                    System.out.println("Invalid choice, please enter again.");
                 }
             }
 
-
-
-
-
+            System.out.println("\n Thank you for using this Car rental system!!");
             }
     }
 
 
     public static void main(String[] args) {
-        System.out.println("hello");
-
-
+        RentalSystem rentalSystem= new RentalSystem();
+        Car car1 = new Car("C001", "Ford", "Mustang", 100.0); // Different base price per day for each car
+        Car car2 = new Car("C002", "Honda", "Accord", 40.0);
+        Car car3 = new Car("C003", "Mahindra", "Thar", 80.0);
+        Car car4 = new Car("C004", "Porsche", "911", 180.0);
+        rentalSystem.addCar(car1);
+        rentalSystem.addCar(car2);
+        rentalSystem.addCar(car3);
+        rentalSystem.addCar(car4);
+        rentalSystem.menu();
     }
 
 
